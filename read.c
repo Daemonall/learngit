544d0cce78d95060cbd783b12ab8b698ab7ef89f
zhangxiaoyou <zhangxyou@139.com>
2017-04-06 15:12:45 +0800

    now

--- read.c
+++ read.c
@@ -4,14 +4,5 @@ Git is free software distributed under the GPL.
 Git has a mutable index called stage.
 Git tracks changs of files.
 
-<<<<<<< HEAD
 Creating a new branch is quick and simple.
 
-piaoyangguohailaokanni
-=======
-Creating a new branch is quick
-
-next is ok or no?
-
-wahaha
->>>>>>> new
