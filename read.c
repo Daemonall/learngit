6beed62229af77388c7967a5a1cf5938ecb4972a
zhangxiaoyou <zhangxyou@139.com>
2017-04-06 14:58:55 +0800

    ok

--- read.c
+++ read.c
@@ -3,10 +3,7 @@ Git is a  distributed version control system.
 Git is free software distributed under the GPL.
 Git has a mutable index called stage.
 Git tracks changs of files.
-学习git of 有时间
-娃哈哈
 
 Creating a new branch is quick & simple.
 
-next
-
+piaoyangguohailaokanni
